33755fb0801ae7f4c36084badaed3ec9a3619f56
Maxohayce <devmax.corl@gmail.com>
2023-01-04 20:57:17 +0100

    Revert "responsive designs added"
    This reverts commit 3298d5cebc5bc5412898bf195561708cebeabee4.

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from './Navbar/navbar';
-export { default as Footer } from './Footer/footer';
-export { default as Faq } from './faq/faq';
-export { default as Carousel } from './carousel/carousel';
-export { default as InvestCard } from './cards/investCard';
-export { default as ProfileCard } from './cards/profileCard';
-export { default as AboutCard } from './cards/aboutCard';
-export {default as Brands } from './brands/brands';
--- src/containers/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Home } from './Home/home';
-export { default as About } from './About/about';
-export { default as Contact } from './Contact/contact';
-export { default as Faqs } from './Faq/faqs';
-export { default as Login } from './Login/login';
-export { default as Register } from './Register/register';
-export {default as Dashboard } from './Dashboard/dashboard';
-export {default as Deposit } from './Dashboard/deposit';
